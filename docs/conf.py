b87f248ff14de0d6efbd871a87ea66f0c3ae1ca8
taxguide <singhparash06@gmail.com>
2026-07-28 16:06:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'ESPN.com/Activate'
+author = 'ESPN.com/Activate'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "ESPN.com/Activate"
